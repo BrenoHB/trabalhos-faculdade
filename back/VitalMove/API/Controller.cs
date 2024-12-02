6ad6cd5490a1e3c723b3c2da13ec4c7c1c820da4
Breno Henrique Busnello <111541433+BrenoHB@users.noreply.github.com>
2024-12-01 22:02:18 -0300

    Token Auth
    Token Auth

---
+++ back/VitalMove/API/Controller.cs
@@ -0,0 +1,98 @@
+using Microsoft.AspNetCore.Mvc;
+using VitalMoveDTO;
+using Util.Login;
+using Services.Alimentacao;
+using Services.AtFisica;
+using Services.IMC;
+using Microsoft.AspNetCore.Authorization;
+using util;
+
+namespace API.Controllers
+{
+    [ApiController]
+
+    public class VitalMoveController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Log(UserLoginDTO credentials)
+        {
+            bool isValid = Login.Authenticate(credentials);
+            if (isValid)
+            {
+                var loginResponse = new LoginResponseDTO { Token = Authenticate.GenerateToken(credentials.CPF) };
+                return Ok(loginResponse);
+            }
+            return BadRequest("Invalid username or password");
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] UserRegisterDTO credentials)
+        {
+            bool isValid = Login.Register(credentials);
+            return isValid ? Ok("Usuário registrado com sucesso!") : BadRequest("Usuário já existente");
+        }
+
+        [HttpPost("PostAlimentacao"), Authorize]
+        public IActionResult PostAlimentacao([FromBody] AlimentacaoResponseDTO dto)
+        {
+            bool isValid = Alimentacao.PostAlimentacao(dto);
+            return isValid ? Ok("Refeição registrada com sucesso") : BadRequest("Erro ao registrar refeição");
+        }
+
+        [HttpPost("GetAlimentacao"), Authorize]
+        public IActionResult GetAlimentacao([FromBody] AlimentacaoRequestDTO dto)
+        {
+            try
+            {
+                var response = Alimentacao.GetAlimentacao(dto);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("PostAtFisica"), Authorize]
+        public IActionResult PostAtFisica([FromBody] AtFisicaResponseDTO dto)
+        {
+            bool isValid = AtFisica.PostAtFisica(dto);
+            return isValid ? Ok("Atividade física registrada com sucesso") : BadRequest("Erro ao registrar atividade física");
+        }
+
+        [HttpPost("GetAtFisica"), Authorize]
+        public IActionResult GetAtFisica([FromBody] AtFisicaRequestDTO dto)
+        {
+            try
+            {
+                var response = AtFisica.GetAtFisica(dto);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("PostIMC"), Authorize]
+        public IActionResult PostIMC([FromBody] IMCResponseDTO dto)
+        {
+            bool isValid = IMC.PostIMC(dto);
+            return isValid ? Ok("IMC registrado com sucesso") : BadRequest("Erro ao registrar IMC");
+        }
+
+        [HttpPost("GetIMC"), Authorize]
+        public IActionResult GetIMC([FromBody] IMCRequestDTO dto)
+        {
+            try
+            {
+                var response = IMC.GetIMC(dto);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
--- back/VitalMove/Program.cs
+++ back/VitalMove/Program.cs
@@ -1,112 +1,55 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using VitalMoveDTO;
-using Util.Login;
-using Services.Alimentacao;
-using Services.AtFisica;
-using Services.IMC;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configuração dos serviços
+// Adicione serviços ao contêiner
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+// Configuração de autenticação e autorização
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("SecretKey") ?? throw new InvalidOperationException("SecretKey não configurada"))
+            )
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+// Configuração de CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowSpecificOrigins", policy =>
+    options.AddPolicy("AllowAll", builder =>
     {
-        policy.WithOrigins("http://localhost:3000") // Endereço do front-end
-              .AllowAnyHeader()
-              .AllowAnyMethod();
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
     });
 });
 
 var app = builder.Build();
 
-
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
-
-app.UseCors("AllowSpecificOrigins");
-
-// Rotas
-app.MapPost("/login", (UserLoginDTO credentials) =>
+// Middleware
+app.UseCors("AllowAll");
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseRouting();
+app.UseAuthentication(); // Middleware de autenticação
+app.UseAuthorization();  // Middleware de autorização
+app.UseEndpoints(endpoints =>
 {
-    bool isValid = Login.Authenticate(credentials);
-    if(isValid){
-       LoginResponseDTO LoginResponse = new  LoginResponseDTO();
-       LoginResponse.Token = "1";
-       return Results.Ok(LoginResponse);
-
-    }
-
-    return Results.BadRequest("Invalid username or password");
-
-}).WithName("Login");
-
-app.MapPost("/register", (UserRegisterDTO credentials) =>
-{
-    bool isValid = Login.Register(credentials);
-    return isValid ? Results.Ok("Usuário registrado com sucesso!") : Results.BadRequest("Usuário já existente");
-}).WithName("Register");
-
-app.MapPost("/PostAlimentacao", (AlimentacaoResponseDTO dto) =>
-{
-    bool isValid = Alimentacao.PostAlimentacao(dto);
-    return isValid ? Results.Ok("Refeição registrada com sucesso") : Results.BadRequest("Erro ao registrar refeição");
-}).WithName("PostAlimentacao");
-
-app.MapPost("/GetAlimentacao", (AlimentacaoRequestDTO dto) =>
-{
-    try
-    {
-        var response = Alimentacao.GetAlimentacao(dto);
-        return Results.Ok(response);
-    }
-    catch (Exception ex)
-    {
-        return Results.BadRequest(ex.Message);
-    }
-}).WithName("GetAlimentacao");
-
-app.MapPost("/PostAtFisica", (AtFisicaResponseDTO dto) =>
-{
-    bool isValid = AtFisica.PostAtFisica(dto);
-    return isValid ? Results.Ok("Atividade física registrada com sucesso") : Results.BadRequest("Erro ao registrar atividade física");
-}).WithName("PostAtFisica");
-
-app.MapPost("/GetAtFisica", (AtFisicaRequestDTO dto) =>
-{
-    try
-    {
-        var response = AtFisica.GetAtFisica(dto);
-        return Results.Ok(response);
-    }
-    catch (Exception ex)
-    {
-        return Results.BadRequest(ex.Message);
-    }
-}).WithName("GetAtFisica");
-
-app.MapPost("/PostIMC", (IMCResponseDTO dto) =>
-{
-    bool isValid = IMC.PostIMC(dto);
-    return isValid ? Results.Ok("IMC registrado com sucesso") : Results.BadRequest("Erro ao registrar IMC");
-}).WithName("PostIMC");
-
-app.MapPost("/GetIMC", (IMCRequestDTO dto) =>
-{
-    try
-    {
-        var response = IMC.GetIMC(dto);
-        return Results.Ok(response);
-    }
-    catch (Exception ex)
-    {
-        return Results.BadRequest(ex.Message);
-    }
-}).WithName("GetIMC");
+    endpoints.MapControllers();
+});
 
 app.Run();
